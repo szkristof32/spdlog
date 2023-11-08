bae21b65ad8e04c78a8382cccd09adcd02506c97
szkristof32 <kristofreg@gmail.com>
2023-11-08 12:18:15 +0100

    Added premake5.lua

---
+++ premake5.lua
@@ -0,0 +1,46 @@
+project "spdlog"
+	kind "StaticLib"
+	language "C++"
+	cppdialect "C++20"
+	staticruntime "off"
+	
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+	
+	files
+	{
+		"src/**.cpp",
+		"include/**.h"
+	}
+	
+	includedirs
+	{
+		"include"
+	}
+	
+	defines
+	{
+		"SPDLOG_COMPILED_LIB"
+	}
+
+	filter "system:windows"
+		systemversion "latest"
+		defines
+		{
+			"SPDLOG_WCHAR_TO_UTF8_SUPPORT"
+		}
+		
+	filter "system:linux"
+		systemversion "latest"
+		
+	filter "configurations:Debug"
+		runtime "Debug"
+		symbols "on"
+		
+	filter "configurations:Release"
+		runtime "Release"
+		optimize "on"
+		
+	filter "configurations:Dist"
+		runtime "Release"
+		optimize "on"
